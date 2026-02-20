4b015880121eeac5f1062ef1158c20b7235e8476
Gurt <leonardcuptor@gmail.com>
2026-02-20 17:49:40 -0500

    vendredi commit

---
+++ Assets/Fil.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Fil : MonoBehaviour
+{
+    bool sens; //true : a->b, false : b->a
+    Vector3 posA;
+    Vector3 posB;
+    GameObject objetA;
+    bool attacheA; //Chaque objet a deux points d'attache le fil peut s'attacher au point A (true) ou B (false)
+    GameObject objetB;
+    bool attacheB;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Pile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Pile : MonoBehaviour
+{
+    float electromotence;
+    bool pointAOccup; //Si le point a un fil qui sort
+    bool pointBOccup;
+    bool sens;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
